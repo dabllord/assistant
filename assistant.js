8545ea3b63ec9f2cefaa8191f3a010f03992c687
Marilyn Monroe <dablvin767@gmail.com>
2025-07-05 00:25:10 +0300

    2.0.0

--- assistant.js
+++ assistant.js
@@ -1,17 +1,25 @@
 const script = registerScript({
   name: "FunTime-Assistant",
-  version: "1.0.0",
+  version: "2.0.0",
   authors: ["Misha Sigma Gucci(tg - @mishasigmagucci)"]
 });
 
 const ClientChatKt = Java.type("net.ccbluex.liquidbounce.utils.client.ClientChat")
 const Severity = Java.type("net.ccbluex.liquidbounce.event.events.NotificationEvent.Severity")
 const Slots = Java.type("net.ccbluex.liquidbounce.utils.inventory.Slots")
-const SilentHotbar = Java.type("net.ccbluex.liquidbounce.utils.client.SilentHotbar")
+const SilentHotbar = Java.type("net.ccbluex.liquidbounce.utils.client.SilentHotbar").INSTANCE
 const InventoryUtilsKt = Java.type("net.ccbluex.liquidbounce.utils.inventory.InventoryUtilsKt")
 const Items = Java.type("net.minecraft.item.Items")
 const DataComponentTypes = Java.type("net.minecraft.component.DataComponentTypes")
 const Colors = Java.type("net.minecraft.util.Colors")
+const ItemSlotType = Java.type("net.ccbluex.liquidbounce.features.module.modules.player.invcleaner.ItemSlotType")
+const HandType = Java.type("net.minecraft.util.Hand")
+const UpdateSelectedSlotC2SPacket = Java.type("net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket")
+const CollectionsKt = Java.type("kotlin.collections.CollectionsKt")
+const ClickInventoryAction = Java.type("net.ccbluex.liquidbounce.utils.inventory.ClickInventoryAction").Companion
+const PlayerInventoryConstraints = Java.type("net.ccbluex.liquidbounce.utils.inventory.PlayerInventoryConstraints")
+const HotbarItemSlot = Java.type("net.ccbluex.liquidbounce.utils.inventory.HotbarItemSlot")
+const Priority = Java.type("net.ccbluex.liquidbounce.utils.kotlin.Priority")
 
 script.registerModule({
   name: "Assistant",
@@ -48,12 +56,31 @@ script.registerModule({
       default: "key.keyboard.unknown"
     }),
 
+    swap_delay: Setting.intRange({
+      name: "Swap delay",
+      default: [0, 0],
+      range: [0, 20]
+    }),
+
     ah_helper: Setting.boolean({
       name: "Auction Helper",
       default: false
+    }),
+
+    smart_ah: Setting.boolean({
+      name: "Smart AH Helper",
+      default: true
     })
   }
 }, (mod) => {
+  mod.on("enable", (event) => {
+    Client.displayChatMessage("disorientation - Дезориентация")
+    Client.displayChatMessage("dust - Явная пыль")
+    Client.displayChatMessage("trap - Трапка")
+    Client.displayChatMessage("aura - Божья аура")
+    Client.displayChatMessage("tornado - Огненный смерч")
+    Client.displayChatMessage("layer - Пласт")
+  })
 
   function extract_item_price(str) {
     return parseFloat(str.replaceAll(",", "").match(/\$([\d,]+)/)[1])
@@ -78,6 +105,24 @@ script.registerModule({
     }
   }
 
+  class itemSearchResult {
+    constructor(slot, count) {  
+      this.slot = slot
+      this.count = count
+    }
+  }
+  
+  const constraints = new PlayerInventoryConstraints()
+
+  constraints.startDelay = mod.settings.swap_delay
+  constraints.clickDelay = mod.settings.swap_delay
+  constraints.closeDelay = mod.settings.swap_delay
+
+
+  function doSwapToHotbat(schedule_inventory_action_event, from, to) {  
+    schedule_inventory_action_event.schedule(constraints, ClickInventoryAction.performSwap(null, from, to), Priority.IMPORTANT_FOR_USAGE_1)
+  }
+  
   lowest_price_item_pos = new vector(0, 0)
   mod.on("playerTick", () => {
     const screen = mc.currentScreen
@@ -98,10 +143,13 @@ script.registerModule({
           return line.getString().toLowerCase().contains("$ ценa")
         }).forEach(line => {
           price = extract_item_price(line.getString().toLowerCase())
+          if (mod.settings.smart_ah) {
+            price /= slot.getStack().getCount() 
+          }
           if (price < lowest_price) {
             lowest_price = price
             lowest_price_item_pos.x = slot.x
-            lowest_price_item_pos.y = slot.yв
+            lowest_price_item_pos.y = slot.y
           }
         })
       }
@@ -125,72 +173,287 @@ script.registerModule({
     context.getMatrices().pop()
   })
 
-  mod.on("key", (event) => {
-    if (event.getAction() === 1) {
-      if (event.getKey().getTranslationKey() == mod.settings.disorientation.value) {
-        const slot = Slots.Hotbar.findSlot(Items.ENDER_EYE)
-        if (slot) {
-          SilentHotbar.INSTANCE.selectSlotSilently(this, slot, 1)
-          InventoryUtilsKt.useHotbarSlotOrOffhand(slot, 1, mc.player.yaw, mc.player.pitch)
-          ClientChatKt.notification(`FT Assistant`, `Дезориентация использована`, Severity.SUCCESS);
-        } else {
-          ClientChatKt.notification(`FT Assistant`, `Дезориентация не найдена в хотбаре`, Severity.ERROR);
+  const action_type = {
+    none: 0,
+    pickup_from_inventory: 1,
+    use_from_hotbar: 2,
+    back_to_inventory: 3
+  }
+
+  const helper_types = {
+    disorientation: Items.ENDER_EYE,
+    dust: Items.SUGAR,
+    trap: Items.NETHERITE_SCRAP,
+    aura: Items.PHANTOM_MEMBRANE,
+    tornado: Items.FIRE_CHARGE,
+    layer: Items.DRIED_KELP
+  }
+
+  use_disorientation = false
+  use_dust = false
+  use_trap = false
+  use_aura = false
+  use_tornado = false
+  use_layer = false
+  const slot_to_swap = new HotbarItemSlot(8)
+  old_slot = null
+  current_action = action_type.none
+  mod.on("scheduleInventoryAction", (event) => {
+    if (use_disorientation) {
+      if (old_slot == null) {
+        old_slot = Slots.All.findSlot(helper_types.disorientation)
+        if (!old_slot) {
+          use_disorientation = false
+          ClientChatKt.notification("FT Assistant", "Дезориентация не найдена", Severity.ERROR)
+          return
         }
+      }
+
+      switch (current_action) {
+        case action_type.none:
+          current_action = action_type.pickup_from_inventory
+          return
+          break;
+
+        case action_type.pickup_from_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          current_action = action_type.use_from_hotbar
+          return
+          break;
+        
+        case action_type.use_from_hotbar:
+          SilentHotbar.selectSlotSilently(this, slot_to_swap, 1)
+          InventoryUtilsKt.useHotbarSlotOrOffhand(slot_to_swap, 1, mc.player.yaw, mc.player.pitch)
+          current_action = action_type.back_to_inventory
+          return
+          break;
+        
+        case action_type.back_to_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          ClientChatKt.notification("FT Assistant", "Дезориентация использована", Severity.SUCCESS)
+          use_disorientation = false
+          old_slot = null
+          current_action = action_type.none
+          return
+          break;
+      }
+    } else if (use_dust) {
+      if (old_slot == null) {
+        old_slot = Slots.All.findSlot(helper_types.dust)
+        if (!old_slot) {
+          use_dust = false
+          ClientChatKt.notification("FT Assistant", "Явная пыль не найдена", Severity.ERROR)
+          return
+        }
+      }
+
+      switch (current_action) {
+        case action_type.none:
+          current_action = action_type.pickup_from_inventory
+          return
+          break;
+
+        case action_type.pickup_from_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          current_action = action_type.use_from_hotbar
+          return
+          break;
+        
+        case action_type.use_from_hotbar:
+          SilentHotbar.selectSlotSilently(this, slot_to_swap, 1)
+          InventoryUtilsKt.useHotbarSlotOrOffhand(slot_to_swap, 1, mc.player.yaw, mc.player.pitch)
+          current_action = action_type.back_to_inventory
+          return
+          break;
+        
+        case action_type.back_to_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          ClientChatKt.notification("FT Assistant", "Явная пыль использована", Severity.SUCCESS)
+          use_dust = false
+          old_slot = null
+          current_action = action_type.none
+          return
+          break;
+      }
+    } else if (use_trap) {
+      if (old_slot == null) {
+        old_slot = Slots.All.findSlot(helper_types.trap)
+        if (!old_slot) {
+          use_trap = false
+          ClientChatKt.notification("FT Assistant", "Трапка не найдена", Severity.ERROR)
+          return
+        }
+      }
+
+      switch (current_action) {
+        case action_type.none:
+          current_action = action_type.pickup_from_inventory
+          return
+          break;
+
+        case action_type.pickup_from_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          current_action = action_type.use_from_hotbar
+          return
+          break;
+        
+        case action_type.use_from_hotbar:
+          SilentHotbar.selectSlotSilently(this, slot_to_swap, 1)
+          InventoryUtilsKt.useHotbarSlotOrOffhand(slot_to_swap, 1, mc.player.yaw, mc.player.pitch)
+          current_action = action_type.back_to_inventory
+          return
+          break;
+        
+        case action_type.back_to_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          ClientChatKt.notification("FT Assistant", "Трапка использована", Severity.SUCCESS)
+          use_trap = false
+          old_slot = null
+          current_action = action_type.none
+          return
+          break;
+      }
+    } else if (use_aura) {
+      if (old_slot == null) {
+        old_slot = Slots.All.findSlot(helper_types.aura)
+        if (!old_slot) {
+          use_aura = false
+          ClientChatKt.notification("FT Assistant", "Божья аура не найдена", Severity.ERROR)
+          return
+        }
+      }
+
+      switch (current_action) {
+        case action_type.none:
+          current_action = action_type.pickup_from_inventory
+          return
+          break;
+
+        case action_type.pickup_from_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          current_action = action_type.use_from_hotbar
+          return
+          break;
+        
+        case action_type.use_from_hotbar:
+          SilentHotbar.selectSlotSilently(this, slot_to_swap, 1)
+          InventoryUtilsKt.useHotbarSlotOrOffhand(slot_to_swap, 1, mc.player.yaw, mc.player.pitch)
+          current_action = action_type.back_to_inventory
+          return
+          break;
+        
+        case action_type.back_to_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          ClientChatKt.notification("FT Assistant", "Божья аура использована", Severity.SUCCESS)
+          use_aura = false
+          old_slot = null
+          current_action = action_type.none
+          return
+          break;
+      }
+    } else if (use_tornado) {
+      if (old_slot == null) {
+        old_slot = Slots.All.findSlot(helper_types.tornado)
+        if (!old_slot) {
+          use_tornado = false
+          ClientChatKt.notification("FT Assistant", "Огненный смерч не найден", Severity.ERROR)
+          return
+        }
+      }
+
+      switch (current_action) {
+        case action_type.none:
+          current_action = action_type.pickup_from_inventory
+          return
+          break;
+
+        case action_type.pickup_from_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          current_action = action_type.use_from_hotbar
+          return
+          break;
+        
+        case action_type.use_from_hotbar:
+          SilentHotbar.selectSlotSilently(this, slot_to_swap, 1)
+          InventoryUtilsKt.useHotbarSlotOrOffhand(slot_to_swap, 1, mc.player.yaw, mc.player.pitch)
+          current_action = action_type.back_to_inventory
+          return
+          break;
+        
+        case action_type.back_to_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          ClientChatKt.notification("FT Assistant", "Огненный смерч использован", Severity.SUCCESS)
+          use_tornado = false
+          old_slot = null
+          current_action = action_type.none
+          return
+          break;
+      }
+    } else if (use_layer) {
+      if (old_slot == null) {
+        old_slot = Slots.All.findSlot(helper_types.layer)
+        if (!old_slot) {
+          use_layer = false
+          ClientChatKt.notification("FT Assistant", "Пласт не найден", Severity.ERROR)
+          return
+        }
+      }
+
+      switch (current_action) {
+        case action_type.none:
+          current_action = action_type.pickup_from_inventory
+          return
+          break;
+
+        case action_type.pickup_from_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          current_action = action_type.use_from_hotbar
+          return
+          break;
+        
+        case action_type.use_from_hotbar:
+          SilentHotbar.selectSlotSilently(this, slot_to_swap, 1)
+          InventoryUtilsKt.useHotbarSlotOrOffhand(slot_to_swap, 1, mc.player.yaw, mc.player.pitch)
+          current_action = action_type.back_to_inventory
+          return
+          break;
+        
+        case action_type.back_to_inventory:
+          doSwapToHotbat(event, old_slot, slot_to_swap)
+          ClientChatKt.notification("FT Assistant", "Пласт использован", Severity.SUCCESS)
+          use_layer = false
+          old_slot = null
+          current_action = action_type.none
+          return
+          break;
+      }
+    }
+  })
+
+  mod.on("key", (event) => {
+    if (event.getAction() == 1 && mod.enabled) {
+      if (event.getKey().getTranslationKey() == mod.settings.disorientation.value) {
+        use_disorientation = true
       }
       
       if (event.getKey().getTranslationKey() == mod.settings.dust.value) {
-        const slot = Slots.Hotbar.findSlot(Items.SUGAR)
-        if (slot) {
-          SilentHotbar.INSTANCE.selectSlotSilently(this, slot, 1)
-          InventoryUtilsKt.useHotbarSlotOrOffhand(slot, 1, mc.player.yaw, mc.player.pitch)
-          ClientChatKt.notification(`FT Assistant`, `Явная пыль использована`, Severity.SUCCESS);
-        } else {
-          ClientChatKt.notification(`FT Assistant`, `Явная пыль не найдена в хотбаре`, Severity.ERROR);
-        }
+        use_dust = true
       }
 
       if (event.getKey().getTranslationKey() == mod.settings.trap.value) {
-        const slot = Slots.Hotbar.findSlot(Items.NETHERITE_SCRAP)
-        if (slot) {
-          SilentHotbar.INSTANCE.selectSlotSilently(this, slot, 1)
-          InventoryUtilsKt.useHotbarSlotOrOffhand(slot, 1, mc.player.yaw, mc.player.pitch)
-          ClientChatKt.notification(`FT Assistant`, `Трапка использована`, Severity.SUCCESS);
-        } else {
-          ClientChatKt.notification(`FT Assistant`, `Трапка не найдена в хотбаре`, Severity.ERROR);
-        }
+        use_trap = true
       }
 
       if (event.getKey().getTranslationKey() == mod.settings.aura.value) {
-        const slot = Slots.Hotbar.findSlot(Items.PHANTOM_MEMBRANE)
-        if (slot) {
-          SilentHotbar.INSTANCE.selectSlotSilently(this, slot, 1)
-          InventoryUtilsKt.useHotbarSlotOrOffhand(slot, 1, mc.player.yaw, mc.player.pitch)
-          ClientChatKt.notification(`FT Assistant`, `Божья аура использована`, Severity.SUCCESS);
-        } else {
-          ClientChatKt.notification(`FT Assistant`, `Божья аура не найдена в хотбаре`, Severity.ERROR);
-        }
+        use_aura = true
       }
 
       if (event.getKey().getTranslationKey() == mod.settings.tornado.value) {
-        const slot = Slots.Hotbar.findSlot(Items.FIRE_CHARGE)
-        if (slot) {
-          SilentHotbar.INSTANCE.selectSlotSilently(this, slot, 1)
-          InventoryUtilsKt.useHotbarSlotOrOffhand(slot, 1, mc.player.yaw, mc.player.pitch)
-          ClientChatKt.notification(`FT Assistant`, `Огненый смерч использован`, Severity.SUCCESS);
-        } else {
-          ClientChatKt.notification(`FT Assistant`, `Огненый смерч не найден в хотбаре`, Severity.ERROR);
-        }
+        use_tornado = true
       }
 
       if (event.getKey().getTranslationKey() == mod.settings.layer.value) {
-        const slot = Slots.Hotbar.findSlot(Items.DRIED_KELP)
-        if (slot) {
-          SilentHotbar.INSTANCE.selectSlotSilently(this, slot, 1)
-          InventoryUtilsKt.useHotbarSlotOrOffhand(slot, 1, mc.player.yaw, mc.player.pitch)
-          ClientChatKt.notification(`FT Assistant`, `Пласт использован`, Severity.SUCCESS);
-        } else {
-          ClientChatKt.notification(`FT Assistant`, `Пласт не найден в хотбаре`, Severity.ERROR);
-        }
+        use_layer = true
       }
     }
   })
